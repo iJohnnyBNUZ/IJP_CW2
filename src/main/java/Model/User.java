8a72284d51553fb66d0f3a272b0d05bf066b58eb
Yang Yang <s1814353@sms.ed.ac.uk>
2019-06-15 16:50:45 +0100

    blank methods for User class

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,49 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private List<Item> bag;
+    private Location currentLocation;
+
+    public List<Item> getBag() {
+        return bag;
+    }
+
+    public void setBag(List<Item> bag) {
+        this.bag = bag;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public List<String> listBagItems(){
+        List<String> bagItems = new ArrayList<>();
+        for(Item item: bag){
+            bagItems.add(item.getItemName());
+        }
+        return bagItems;
+    }
+
+    public void addItem(Item item){
+
+    }
+
+    public void removeItem(Item item){
+
+    }
+
+    public void moveInDirection(int angle){
+
+    }
+
+    public void updateView(){
+
+    }
 }
