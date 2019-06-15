ee11890562d7e5f050e1f9d094f7328b763ea79c
Yang Yang <s1814353@sms.ed.ac.uk>
2019-06-15 16:43:16 +0100

    complete World Class

--- src/main/java/Model/World.java
+++ src/main/java/Model/World.java
@@ -1,4 +1,24 @@
 package Model;
 
+import java.util.List;
+
 public class World {
+    private List<Location> allLocations;
+    private List<User> allUsers;
+
+    public List<Location> getAllLocations() {
+        return allLocations;
+    }
+
+    public void setAllLocations(List<Location> allLocations) {
+        this.allLocations = allLocations;
+    }
+
+    public List<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(List<User> allUsers) {
+        this.allUsers = allUsers;
+    }
 }
