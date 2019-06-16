294c907e6370012248b321cffbbb13cff64587f6
Yang Yang <s1814353@sms.ed.ac.uk>
2019-06-16 20:35:08 +0100

    Fix bugs

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -58,7 +58,7 @@ public class User {
 //        Update location view
         LocationView.getLocationView().updateLocation(currentLocation.getLocationName(), arrowAngles);
 //        Update item view
-        ItemView.getItemView().updateItems(currentLocation.getIemsName());
+        ItemView.getItemView().updateItems(currentLocation.getItems());
 //        Update bag view
         List<String> bagItems = new LinkedList<>();
         for (Item item: bag){
