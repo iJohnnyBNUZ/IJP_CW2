c61f5d595e28175ceef981622055b5aa0768870d
Yang Yang <s1814353@sms.ed.ac.uk>
2019-06-16 10:14:57 +0100

    Update item class code

--- src/main/java/Model/Item.java
+++ src/main/java/Model/Item.java
@@ -10,11 +10,27 @@ public class Item {
         this.ItemPositionY = y;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
     public int getItemPositionX() {
         return ItemPositionX;
     }
 
+    public void setItemPositionX(int itemPositionX) {
+        ItemPositionX = itemPositionX;
+    }
+
     public int getItemPositionY() {
         return ItemPositionY;
+    }
+
+    public void setItemPositionY(int itemPositionY) {
+        ItemPositionY = itemPositionY;
     }
 }
